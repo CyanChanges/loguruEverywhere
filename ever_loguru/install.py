310b79502c5e97a3e0ad61576bccaaef48a98034
Cyan <68551684+chinese-cyq20100313@users.noreply.github.com>
2023-05-02 16:07:31 +0800

    fix

--- ever_loguru/install.py
+++ ever_loguru/install.py
@@ -1,5 +1,7 @@
+import functools
 import sys
 from .handler import LoguruLoggingHandler
+from .mask import FakeLogger
 from .wrapper import AsyncLoggingLoguruWrapper, LoggingLoguruWrapper
 
 callHandlers_func = sys.modules['logging'].Logger.callHandlers
@@ -23,22 +25,33 @@ def _callHandlers_wrapper(original, record):
 def update_exist():
     import logging
     for logger_name in logging.Logger.manager.loggerDict.keys():
-        logging.Logger.manager.loggerDict[
-            logger_name
-        ] = LoggingLoguruWrapper()
+        logging.Logger.manager.loggerDict.update(
+            {logger_name: LoggingLoguruWrapper()}
+        )
 
+
+def update_exist_instancecheck():
+    import logging
+    for logger_name in logging.Logger.manager.loggerDict.keys():
+        logging_logger = logging.Logger.manager.loggerDict[
+            logger_name
+        ]
+        setattr(
+            logging_logger,
+            "__instancecheck__",
+            functools.partial(FakeLogger.__instancecheck__, logging_logger)
+        )
 
 # noinspection PyPep8Naming
 def install():
     sys.modules['logging'].Logger.callHandlers = _callHandlers_wrapper
 
-
 # noinspection PyPep8Naming
-def install_class(replace_exist: bool = False):
+def install_force(replace_exist: bool = False):
     """
-    Install which replace Logger class
+    Install which replace Logger class that may often break something during instancecheck
     :param replace_exist: replace_exist
-    :return: 
+    :return:
     """
     _Logger = getattr(sys.modules['logging'], "Logger")
     setattr(sys.modules['logging'], "Logger", LoggingLoguruWrapper)
@@ -47,10 +60,11 @@ def install_class(replace_exist: bool = False):
 
     if replace_exist:
         update_exist()
-
+    else:
+        update_exist_instancecheck()
 
 # noinspection PyPep8Naming
-def install_force(replace_exist: bool = False):
+def install_class(replace_exist: bool = False):
     """
     Install for manager.getLogger, manager.loggerClass that may break some modules
     :param replace_exist: Replace exist logger
@@ -60,9 +74,11 @@ def install_force(replace_exist: bool = False):
     sys.modules['logging'].Logger.manager.getLogger = LoggingLoguruWrapper
     sys.modules['logging'].Logger.manager.loggerClass = LoggingLoguruWrapper
     setattr(sys.modules['logging'].Logger.manager, "loggerClass", LoggingLoguruWrapper)
+
     if replace_exist:
         update_exist()
-
+    else:
+        update_exist_instancecheck()
 
 # noinspection PyPep8Naming
 def remove():
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='EverLoguru',
-    version='1.1.4',
+    version='1.1.5',
     packages=['ever_loguru'],
     url='',
     license='',
